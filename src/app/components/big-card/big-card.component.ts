7dd79477ae9a6df24e97799205713fa9a20f0e0a
vianarafaa <vianarafaa@gmail.com>
2023-09-28 13:06:13 -0300

    responsivity and dynamic components

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,11 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
   templateUrl: './big-card.component.html',
-  styleUrls: ['./big-card.component.css']
+  styleUrls: ['./big-card.component.css', './big-card.responsive.component.css']
 })
 export class BigCardComponent implements OnInit {
+
+  @Input()
+  photoCover:string = ''
+  @Input()
+  cardDate:string = ''
+  @Input()
+  cardTitle:string = ''
+  @Input()
+  cardDescription:string = ''
 
   constructor() { }
 
--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,11 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
   templateUrl: './small-card.component.html',
-  styleUrls: ['./small-card.component.css']
+  styleUrls: ['./small-card.component.css', './small-card.responsive.component.css']
 })
 export class SmallCardComponent implements OnInit {
+
+  @Input()
+  photoCover:string = ''
+  @Input()
+  cardDate:string = ''
+  @Input()
+  cardTitle:string = ''
 
   constructor() { }
 
